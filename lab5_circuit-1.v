855020bfe7e8c19e863afe50c17eac876b0e1de6
Shayan (Sean) Taheri <shayan.taheri@knights.ucf.edu>
2020-06-01 13:06:47 -0400

    Add files via upload

---
+++ lab5_circuit-1.v
@@ -0,0 +1,24 @@
+///////////////////////////
+// ECE6930 2014 Spring  //
+// LAB. 5 circuit-1    //
+// Verilog format     //
+///////////////////////
+
+// library used /opt/software/cadence/library/tcbn45nm/verilog/HVT/tcbn45gsbwphvt.v
+
+
+module circuit_1 ( clk, N1, N2, PO1, PO2 );
+  input clk, N1, N2;
+  output PO1, PO2;
+  wire   ff1, ff2, ff3, ff4, n2, n3, n4;
+
+  DFQD1BWPHVT ff4_reg ( .D(ff1), .CP(clk), .Q(ff4) );
+  DFQD1BWPHVT ff2_reg ( .D(n4), .CP(clk), .Q(ff2) );
+  DFQD1BWPHVT ff3_reg ( .D(n3), .CP(clk), .Q(ff3) );
+  DFQD1BWPHVT ff1_reg ( .D(N2), .CP(clk), .Q(ff1) );
+  ND2D1BWPHVT U8 ( .A1(ff2), .A2(ff1), .ZN(n3) );
+  OR2XD1BWPHVT U9 ( .A1(ff1), .A2(N1), .Z(n4) );
+  OR2XD1BWPHVT U10 ( .A1(ff4), .A2(n3), .Z(PO2) );
+  INVD1BWPHVT U11 ( .I(N1), .ZN(n2) );
+  OR3XD1BWPHVT U12 ( .A1(n2), .A2(ff3), .A3(n3), .Z(PO1) );
+endmodule
---
+++ lab5_circuit-2.v
@@ -0,0 +1,24 @@
+///////////////////////////
+// ECE6930 2014 Spring  //
+// LAB. 5 circuit-2    //
+// Verilog format     //
+///////////////////////
+
+// library used /opt/software/cadence/library/tcbn45nm/verilog/HVT/tcbn45gsbwphvt.v
+
+module circuit_2 ( clk, CNT, PO );
+  input clk, CNT;
+  output PO;
+  wire   n1, n2, n3, n4, n5, n6, n7, n8;
+
+  DFQD1BWPHVT ff1_reg ( .D(n5), .CP(clk), .Q(n1) );
+  DFQD1BWPHVT ff2_reg ( .D(n8), .CP(clk), .Q(PO) );
+  XOR2D1BWPHVT U1 ( .A1(n1), .A2(CNT), .Z(n2) );
+  OR2XD1BWPHVT U2 ( .A1(n1), .A2(CNT), .Z(n3) );
+  AN2XD1BWPHVT U3 ( .A1(n2), .A2(n4), .Z(n5) );
+  ND2D1BWPHVT U4 ( .A1(n3), .A2(PO), .ZN(n4) );
+  AN2XD1BWPHVT U5 ( .A1(n1), .A2(CNT), .Z(n6) );
+  XOR2D1BWPHVT U6 ( .A1(PO), .A2(n6), .Z(n7) );
+  AN2XD1BWPHVT U7 ( .A1(n7), .A2(n4), .Z(n8) );
+
+endmodule
